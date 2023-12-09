98de03446c09d48a5e726281f96f377011c2da2d
youpiteron <oyushchenko@kse.org.ua>
2023-12-09 16:37:10 +0200

    Bakery analysis in half-year dataset and quarters analysis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-#import seaborn as sns
+import scipy.stats as stats
 
 types = ['Coffee', 'Bakery', 'Tea', 'Drinking Chocolate']
 
@@ -8,21 +8,23 @@ types = ['Coffee', 'Bakery', 'Tea', 'Drinking Chocolate']
 def tables_for_product(product: str):
     df = pd.read_csv("statistic_project.csv", sep=';')
     df = df[df['product_category'] == product]
-    timeFrame = pd.DataFrame(columns=['time', 'qty'])
+    df['unit_price'] = df['unit_price'].apply(lambda x: float(x.replace(',', '.')))
+
+    timeFrame = pd.DataFrame(columns=['time', 'revenue'])
     timeFrame['time'] = pd.to_timedelta(df['transaction_time']).dt.components['hours'].astype(int)
-    timeFrame['qty'] = df['transaction_qty']
-    timeFrame = timeFrame.groupby('time')['qty'].sum().reset_index()
+    timeFrame['revenue'] = df['transaction_qty']*df['unit_price']
+    timeFrame = timeFrame.groupby('time')['revenue'].sum().reset_index()
     timePlt = plt.subplot(2, 1, 1)
-    cont = timePlt.bar(timeFrame['time'], timeFrame['qty'])
+    cont = timePlt.bar(timeFrame['time'], timeFrame['revenue'])
     timePlt.bar_label(cont)
     plt.title(product)
 
-    dayFrame = pd.DataFrame(columns=['day', 'qty'])
+    dayFrame = pd.DataFrame(columns=['day', 'revenue'])
     dayFrame['day'] = pd.to_datetime(df['transaction_date'], format='%d.%m.%Y').dt.day.astype(int)
-    dayFrame['qty'] = df['transaction_qty']
-    dayFrame = dayFrame.groupby('day')['qty'].sum().reset_index()
+    dayFrame['revenue'] = df['transaction_qty']*df['unit_price']
+    dayFrame = dayFrame.groupby('day')['revenue'].sum().reset_index()
     dayPlt = plt.subplot(2, 1, 2)
-    cont = dayPlt.bar(dayFrame['day'], dayFrame['qty'])
+    cont = dayPlt.bar(dayFrame['day'], dayFrame['revenue'])
     dayPlt.bar_label(cont)
     plt.show()
 
@@ -35,26 +37,28 @@ for type in types:
 
 def tables_for_all_products():
     df = pd.read_csv("statistic_project.csv", sep=';')
-    timeFrame = pd.DataFrame(columns=['time', 'qty'])
+    df['unit_price'] = df['unit_price'].apply(lambda x: float(x.replace(',', '.')))
+
+    timeFrame = pd.DataFrame(columns=['time', 'revenue'])
     timeFrame['time'] = pd.to_timedelta(df['transaction_time']).dt.components['hours'].astype(int)
-    timeFrame['qty'] = df['transaction_qty']
-    timeFrame = timeFrame.groupby('time')['qty'].sum().reset_index()
+    timeFrame['revenue'] = df['transaction_qty']*df['unit_price']
+    timeFrame = timeFrame.groupby('time')['revenue'].sum().reset_index()
     timePlt = plt.subplot(2, 1, 1)
-    cont = timePlt.bar(timeFrame['time'], timeFrame['qty'])
+    cont = timePlt.bar(timeFrame['time'], timeFrame['revenue'])
     timePlt.bar_label(cont)
     plt.title('All products')
 
-    dayFrame = pd.DataFrame(columns=['day', 'qty'])
+    dayFrame = pd.DataFrame(columns=['day', 'revenue'])
     dayFrame['day'] = pd.to_datetime(df['transaction_date'], format='%d.%m.%Y').dt.day.astype(int)
-    dayFrame['qty'] = df['transaction_qty']
-    dayFrame = dayFrame.groupby('day')['qty'].sum().reset_index()
+    dayFrame['revenue'] = df['transaction_qty']*df['unit_price']
+    dayFrame = dayFrame.groupby('day')['revenue'].sum().reset_index()
     dayPlt = plt.subplot(2, 1, 2)
-    cont = dayPlt.bar(dayFrame['day'], dayFrame['qty'])
+    cont = dayPlt.bar(dayFrame['day'], dayFrame['revenue'])
     dayPlt.bar_label(cont)
     plt.show()
 
 
-tables_for_all_products()
+# tables_for_all_products()
 
 
 def price_from_place():
@@ -76,4 +80,91 @@ def price_from_place():
     print(locationFrame)
 
 
-price_from_place()
+# price_from_place()
+
+
+def calculate_amount_of_product(mean: float, std: float):
+    confidence_level = 0.5
+
+    z_score = stats.norm.ppf((1 + confidence_level) / 2)
+    amount_needed = mean + z_score * std
+
+    return amount_needed
+
+
+def bakery_percents(df):
+    percent_frame = pd.DataFrame(columns=['transaction_qty', 'product_type'])
+    percent_frame['transaction_qty'] = df['transaction_qty'].astype(int)
+    percent_frame['product_type'] = df['product_type']
+    result_sum = percent_frame.groupby(['product_type'])['transaction_qty'].sum().reset_index()
+    total = result_sum['transaction_qty'].sum()
+    print(total)
+    result_sum['percent'] = result_sum['transaction_qty'] / total * 100
+    print(result_sum, '\n')
+
+
+def bakery_analysis():
+    df = pd.read_csv("year_data.csv", sep=';')
+    df = df[df['product_category'] == 'Bakery']
+
+    bakery_percents(df)
+
+    stores = ['Lower Manhattan', 'Hell\'s Kitchen', 'Astoria']
+    for store in stores:
+        store_f = df[df['store_location'] == store]
+
+        store_frame = pd.DataFrame(columns=['transaction_qty', 'date', 'hours'])
+        store_frame['hours'] = pd.to_timedelta(store_f['transaction_time']).dt.components['hours'].astype(int)
+        store_frame['transaction_qty'] = store_f['transaction_qty'].astype(int)
+        store_frame['date'] = pd.to_datetime(store_f['transaction_date'], format='%d.%m.%Y').dt.day.astype(int)
+        result_sum = store_frame.groupby(['date', 'hours'])['transaction_qty'].sum().reset_index()
+        # print(result_sum['transaction_qty'].sum())
+
+        result_std = result_sum.groupby('hours')['transaction_qty'].std().reset_index()
+        result_mean = result_sum.groupby('hours')['transaction_qty'].mean().reset_index()
+
+        result = pd.DataFrame(columns=['hours', 'mean', 'std', 'to_produce'])
+
+        result['hours'] = result_std['hours']
+        result['mean'] = result_mean['transaction_qty']
+        result['std'] = result_std['transaction_qty']
+
+        print(store)
+        bakery_percents(store_f)
+        # print('STD:')
+        # print(result_std, '\n')
+        # print('Mean:')
+        # print(result_mean, '\n')
+        # print('\n')
+
+        for index, row in result.iterrows():
+            result.loc[index, 'to_produce'] = calculate_amount_of_product(row['mean'], row['std'])
+        print(result, '\n')
+
+
+# bakery_analysis()
+
+
+def quarters():
+    df = pd.read_csv("year_data.csv", sep=';')
+    df = df[df['product_category'] == 'Bakery']
+
+    df['date'] = pd.to_datetime(df['transaction_date'], format='%d.%m.%Y')
+    split_date = '2023-03-31 00:00:00'
+    q1 = df[df['date'] <= split_date].copy()
+    q2 = df[df['date'] > split_date].copy()
+    bakery_percents(q1)
+    bakery_percents(q2)
+
+    stores = ['Lower Manhattan', 'Hell\'s Kitchen', 'Astoria']
+    for store in stores:
+        store_df = df[df['store_location'] == store].reset_index(drop=True)
+        q1_store = store_df[store_df['date'] <= split_date].copy()
+        q2_store = store_df[store_df['date'] > split_date].copy()
+
+        print(store)
+        bakery_percents(q1_store)
+        bakery_percents(q2_store)
+
+
+quarters()
